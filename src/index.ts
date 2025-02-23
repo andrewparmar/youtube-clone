d820c5c94f3e6cf722f927e189a05c3c9dade1a1
Andrew Parmar <andrew.parmar@pivotal.aero>
2025-02-23 01:06:05 -0800

    Add storage.ts file and refactor business logic out of index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import ffmpeg from "fluent-ffmpeg"
+import {setupDirectories, convertVideo} from "./storage"
+
+setupDirectories();
 
 const app = express();
 app.use(express.json())
 
-app.post("/process-video", (req, res) => {
+app.post("/process-video", async (req, res) => {
 	console.log(req.body);
 	const inputFilePath = req.body.inputFilePath;
 	const outputFilePath = req.body.outputFilePath;
@@ -13,16 +15,9 @@ app.post("/process-video", (req, res) => {
 		res.status(400).send("Bad Request: Missing file path.")
 	}
 
-	ffmpeg(inputFilePath)
-		.outputOptions("-vf", "scale=-1:360") // 360p
-		.on("end", () => {
-			res.status(200).send("Video processing completed.")
-		})
-		.on("error", (err) => {
-			console.log(`Error: ${err.message}`);
-			res.status(500).send(`Internal Server Error: ${err.message}`);
-		})
-		.save(outputFilePath);
+	await convertVideo(inputFilePath, outputFilePath)
+
+	res.status(200).send("Video processing complete.")
 });
 
 const port = process.env.PORT || 3000;
---
+++ src/storage.ts
@@ -0,0 +1,97 @@
+import {Storage, UploadOptions} from '@google-cloud/storage'
+import ffmpeg from "fluent-ffmpeg";
+import path from 'node:path'
+import * as fs from 'node:fs/promises';
+import {mkdir} from 'node:fs';
+
+const storage = new Storage();
+
+const rawVideoBucketName = ''
+const processedVideoBucketName = ''
+const localRawVideoPath = './raw-videos'
+const localProcessedVideoPath = './processed-videos'
+
+export function setupDirectories() {
+	console.log("Setting up service directories.")
+	ensureDirectoryExists(localRawVideoPath);
+	ensureDirectoryExists(localProcessedVideoPath);
+}
+
+export function convertVideo(rawVideoName: string, processedVideoName: string): Promise<void> {
+	return new Promise((resolve, reject) => {
+		ffmpeg(path.join(localRawVideoPath, rawVideoName))
+			.outputOptions("-vf", "scale=-1:360") // 360p
+			.on("progress", progress => {
+				if (progress.percent) console.log(`Processing: ${progress.percent}% done`)
+			})
+			.on("end", () => {
+				console.log("Video processing completed.");
+				resolve();
+			})
+			.on("error", (err) => {
+				console.log(`Error: ${err.message}`);
+				reject(err);
+			})
+			.save(path.join(localProcessedVideoPath, processedVideoName));
+	})
+}
+
+export async function downloadRawVideo(fileName: string) {
+	const filePath = path.join(localRawVideoPath, fileName)
+	const options = {destination: filePath};
+
+	// Downloads the file
+	await storage.bucket(processedVideoBucketName).file(fileName).download(options);
+
+	console.log(`gs://${rawVideoBucketName}/${fileName} downloaded to ${filePath}.`);
+}
+
+export async function uploadProcessedVideo(fileName: string) {
+	const bucket = storage.bucket(processedVideoBucketName)
+	const options = {destination: fileName} as UploadOptions;
+
+	await bucket.upload(fileName, options);
+	console.log(`${fileName} uploaded to ${processedVideoBucketName}`);
+
+	await bucket.file(fileName).makePublic();
+}
+
+export async function deleteRawVideo(fileName: string) {
+	await deleteFile(path.join(localRawVideoPath, fileName))
+}
+
+export async function deleteProcessedVideo(fileName: string) {
+	await deleteFile(path.join(localProcessedVideoPath, fileName))
+}
+
+async function deleteFile(filePath: string): Promise<void> {
+	try {
+		await fs.unlink(filePath);
+		console.log(`Successfully deleted ${filePath}`)
+	} catch (error) {
+		if (error instanceof Error) {
+			console.log("There was an error:", error.message);
+		} else {
+			console.log("An unknown error occurred:", error);
+		}
+	}
+}
+
+// async function ensureDirectoryExists(dirName: string){
+// 	try {
+// 		await fs.mkdir(dirName);
+// 		console.log(`Created ${dirName}`);
+// 	} catch (error) {
+// 		if (error instanceof Error) {
+// 			console.log("There was an error:", error.message);
+// 		} else {
+// 			console.log("An unknown error occurred:", error);
+// 		}
+// 	}
+// }
+
+function ensureDirectoryExists(dirName: string) {
+	mkdir(dirName, {recursive: true}, (err) => {
+		if (err) throw err;
+	});
+}
